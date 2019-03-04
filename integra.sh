cd8dc17d5f810d553ff63615c19f932f31e5ed0f
Andrian Hartanto <andrianhartanto@outlook.com>
2019-03-04 13:45:13 +0700

    Updated bash script

--- integra.sh
+++ integra.sh
@@ -2,22 +2,25 @@
 # parameter 1 is the ip address of the storage server
 # parameter 2 is the path to the directory
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
-  echo "Incomplete parameters, cannot proceed"
-  exit 1
-else
+  echo "Installing moodledata in local storage"
+  PATH = "/var/moodledata"
+fi
     echo "Application data will be stored on $1:$2"
-    apt install apache2 mysql-client php php-fpm libapache2-mod-php
-    a2enmod proxy_fcgi setenvif
-    a2enconf php7.2-fpm
-    apt install graphviz aspell ghostscript clamav php-pspell php-curl php-gd php-intl php-mysql php-xml php-xmlrpc php-ldap php-zip php-soap php-mbstring
+    apt -y install apache2 mysql-client php php-fpm libapache2-mod-php
+    apt -y install graphviz aspell ghostscript clamav php-pspell php-curl php-gd php-intl php-mysql php-xml php-xmlrpc php-ldap php-zip php-soap php-mbstring
     service apache2 restart
 
     cp -r integra /var/www/html/
     chmod -R 0755 /var/www/html/Integra-LMS
 
-    mkdir /mnt/moodledata
-    mount $1:$2 /mnt/moodledata
+    if [[ -z "$PATH" ]]; then
+      mkdir /mnt/moodledata
+      mount $1:$2 /mnt/moodledata
+    else
+      mkdir $PATH
+      chown -R www-data $PATH
+      chmod -R 777 $PATH
+    fi
 
     crontab -u www-data -e
     */1 * * * * /usr/bin/php  /var/www/html/Integra-LMS/admin/cli/cron.php >/dev/null
-fi
